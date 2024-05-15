474c50db9080d00d73b9f1691359751fa27dea40
Alex Lee <lee.a.esq@gmail.com>
2024-05-15 14:55:32 +0900

    made modifications for bookings view, model, and controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,39 +1,11 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = booking.all
+    @bookings = Booking.all
   end
 
-  def new
-    @booking = Booking.new(booking_params)
-  enx
-
-  def create
-    @user = current_user
-    @costume = Costume.find(params[:costume_id])
-    @booking = Booking.new(booking_params)
-    @booking.costume = @costume
-    @booking.user = @user
-    @booking.save
-    if @booking.id
-      redirect_to costumes_path
-    else
-      render 'new', status: :unprocessable_entity
-    end
+  def show
+    @booking = Booking.find(params[:id])
   end
 
-  def index
-    @user = current_user
-    @bookings = @user.bookings
-  end
 
-def show
-  @booking = Booking.find(params[:id])
-end
-
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:user_id, :costume_id, :start_time, :end_time, :status)
-  end
 end
