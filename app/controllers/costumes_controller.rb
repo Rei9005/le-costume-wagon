50e7c13ec60ebf431a39c138efb1381da0998736
]Hayato Furuhashi <seabasscraze@yahoo.co.jp>
2024-05-15 11:25:32 +0900

    Hayatos stuff which is show and controller for that

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,14 +1,29 @@
 class CostumesController < ApplicationController
 
   def index
+    @costumes = Costume.all
   end
 
   def show
+    @costume = Costume.find(params[:id])
   end
 
   def new
+    @costume = Costume.new
   end
 
   def create
+    @costume = Costume.new(costume_params)
+    if @costume.save
+      redirect_to @costume, notice: 'Costume was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :price, :size, :description, :image)
   end
 end
